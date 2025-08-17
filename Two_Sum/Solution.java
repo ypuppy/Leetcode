e4cc4445a5a41ee1405bb6f3fd5db329b1d42c4e
YinCan <141732996+ypuppy@users.noreply.github.com>
2025-08-17 23:45:13 +0800

    Add Two Sum solution

--- Two_Sum/Solution.java
+++ Two_Sum/Solution.java
@@ -5,8 +5,19 @@ You may assume that each input would have exactly one solution, and you may not 
 
 You can return the answer in any order.
  */
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-
+        Map<Integer, Integer> numToIndex = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (numToIndex.containsKey(complement)) {
+                return new int[] { numToIndex.get(complement), i };
+            }
+            numToIndex.put(nums[i], i);
+        }
+        return new int[0];
     }
 }
